d4accb24d565529b005e81215ac4cef1346e682f
hasin.pramodya <pramodyahasin@gmail.com>
2025-11-09 16:24:24 +0530

    add styles to the movie card component

--- src/components/MovieList/MovieCard.jsx
+++ src/components/MovieList/MovieCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './MovieCard.css'
-import Star from '../../assets/star.png'
+import Star from '../../assets/glowing-star.png'
 
 export const MovieCard = () => {
   return (
@@ -8,9 +8,9 @@ export const MovieCard = () => {
      <img src="https://yesimadesigner.com/wp-content/uploads/2018/06/10.jpg?x29684&x29684&x29684" alt="movie poster" className="movie_poster"/>
      <div className="movie_details">
         <h3 className="movie_details_heading">Movie Name</h3>
-        <div className="movie_date_rate">
+        <div className="align-center movie_date_rate">
             <p>10-20-2020</p>
-            <p>8.0 <img src="{Star}" alt="rating icon" className="card_emoji"/></p>
+            <p>8.0 <img src={Star} alt="rating icon" className="card_emoji"/></p>
         </div>
         <p className='movie_description'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
      </div>
