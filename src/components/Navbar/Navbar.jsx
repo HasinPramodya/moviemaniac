293586e113388890048696bbbb52d7d5b4524f77
hasin.pramodya <pramodyahasin@gmail.com>
2025-11-09 13:29:07 +0530

    add navbar component and it's style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'   
 import "./Navbar.css"
+import Fire from '../../assets/fire.png'
+import Star from '../../assets/glowing-star.png'
+import Party from '../../assets/partying-face.png'
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="navbar">
+      <h1>MovieManic</h1>
+      <div className="navbar_links">
+        <a href="">Popular<img src={Fire} alt="fire emoji" className="navbar_emoji"/></a>
+        <a href="">Top Rated<img src={Star} alt="star emoji" className="navbar_emoji"/></a>
+        <a href="">Upcoimng<img src={Party} alt="party emoji" className="navbar_emoji"/></a>
+      </div>
+    </nav>
+    
+    
   )
 }
 
