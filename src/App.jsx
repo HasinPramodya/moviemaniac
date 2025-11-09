1071bfd4ad3c6c67a8ec4c4a060ba488e94942af
hasin.pramodya <pramodyahasin@gmail.com>
2025-11-09 11:17:49 +0530

    add montserrat google front to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 
+
 function App() {
   
 
   return (
     <>
-    <div className='app'>
+    <div className='app montserrat-heading'>
           <h1>Hello World</h1>
           <h1>Hello My New Project</h1>
     </div>
