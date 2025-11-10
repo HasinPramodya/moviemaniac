f99df6e1e306b6135fe8c891b124661d58cb6892
hasin.pramodya <pramodyahasin@gmail.com>
2025-11-10 20:18:07 +0530

    add dark mode component to the nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,12 +3,14 @@ import "./Navbar.css"
 import Fire from '../../assets/fire.png'
 import Star from '../../assets/glowing-star.png'
 import Party from '../../assets/partying-face.png'
+import DarkMode from '../DarkMode/DarkMode'
 
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>MovieManic</h1>
+      <DarkMode/>
       <div className="navbar_links">
         <a href="">Popular<img src={Fire} alt="fire emoji" className="navbar_emoji"/></a>
         <a href="">Top Rated<img src={Star} alt="star emoji" className="navbar_emoji"/></a>
