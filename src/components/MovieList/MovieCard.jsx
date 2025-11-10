2bee854421acb301f32711bca75b1ee0e752968b
hasin.pramodya <pramodyahasin@gmail.com>
2025-11-10 14:53:33 +0530

    refactor the movieCard component using prop

--- src/components/MovieList/MovieCard.jsx
+++ src/components/MovieList/MovieCard.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import './MovieCard.css'
 import Star from '../../assets/glowing-star.png'
 
-export const MovieCard = () => {
+export const MovieCard = ({movie}) => {
   return (
-   <a href="" className="movie_card">
-     <img src="https://yesimadesigner.com/wp-content/uploads/2018/06/10.jpg?x29684&x29684&x29684" alt="movie poster" className="movie_poster"/>
+   <a href={`https://www.themoviedb.org/movie/${movie.id}`} className="movie_card" target='_blank'>
+     <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="movie poster" className="movie_poster"/>
      <div className="movie_details">
-        <h3 className="movie_details_heading">Movie Name</h3>
+        <h3 className="movie_details_heading">{movie.original_title}</h3>
         <div className="align-center movie_date_rate">
-            <p>10-20-2020</p>
-            <p>8.0 <img src={Star} alt="rating icon" className="card_emoji"/></p>
+            <p>{movie.release_date}</p>
+            <p>{movie.vote_average} <img src={Star} alt="rating icon" className="card_emoji"/></p>
         </div>
-        <p className='movie_description'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <p className='movie_description'>{movie.overview.slice(0,100)+"..."}</p>
      </div>
    </a>
   )
