112849991fa7460356318fea681bd8e177291cc9
hasin.pramodya <pramodyahasin@gmail.com>
2025-11-10 23:03:50 +0530

    implement theme toggle functionality in DarkMode component

--- src/components/DarkMode/DarkMode.jsx
+++ src/components/DarkMode/DarkMode.jsx
@@ -5,11 +5,37 @@ import Sun from "../../assets/Sun.svg?react";
 import Moon from "../../assets/Moon.svg?react";
 
 const DarkMode = () => {
+    const setDarkTheme = () => {
+        document.querySelector("body").setAttribute("data-theme", "dark");
+        localStorage.setItem("selectedTheme", "dark");
+    };
+
+    const setLightTheme = () => {
+        document.querySelector("body").setAttribute("data-theme", "light");
+        localStorage.setItem("selectedTheme", "light");
+    };
+
+    const selectedTheme = localStorage.getItem("selectedTheme");
+
+    if (selectedTheme === "light") {
+        setLightTheme();
+    } else {
+        setDarkTheme();
+    }
+
+    const toggleTheme = (e) => {
+        if (e.target.checked) {
+            setDarkTheme();
+        } else {
+            setLightTheme();
+        }
+    };
     return (
         <div className='dark_mode'>
             <input
                 className='dark_mode_input'
                 type='checkbox'
+                onChange={toggleTheme}
                 id='darkmode-toggle'
             />
             <label className='dark_mode_label' htmlFor='darkmode-toggle'>
